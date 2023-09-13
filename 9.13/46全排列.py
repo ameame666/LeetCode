40ececd6fff7374e2ab73094546c6129650926f7
ameame666 <a2665446972@gmail.com>
2023-09-13 17:33:10 +0800

    9.13 commit

--- 9.13/46全排列.py
+++ 9.13/46全排列.py
@@ -1,2 +1,22 @@
 # https://leetcode.cn/problems/permutations/
 # 回溯算法，注意用一个used判断这个数是否使用过
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        track = []
+        used = [False] * len(nums)
+
+        def backtrack():
+            if len(track) == len(nums):
+                res.append(track.copy())
+            
+            for i in range(len(nums)):
+                if not used[i]:
+                    used[i] = True
+                    track.append(nums[i])
+                    backtrack()
+                    used[i] = False
+                    track.pop()
+        
+        backtrack()
+        return res
--- 9.13/61旋转链表.py
+++ 9.13/61旋转链表.py
@@ -1,2 +1,21 @@
 # https://leetcode.cn/problems/rotate-list/
 # 先遍历链表确定链表的长度，然后需要旋转的长度就是k % length，再把链表首尾相连后在需要旋转的地方断开链表，时间复杂度O(n)
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or not head.next or k == 0:
+            return head
+        length = 1
+        cur = head
+        while cur.next:
+            length += 1
+            cur = cur.next
+        cur.next = head
+
+        k = k % length
+        
+        for i in range(length - k):
+            cur = cur.next
+
+        newhead = cur.next
+        cur.next = None
+        return newhead
